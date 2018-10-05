fe161283dbbae679a5ebbc23f6f46c884ef0a151
WangYanlin <wangyl_a@163.com>
2018-10-05 12:54:24 +0800

    eval(),bit_length()

---
+++ grammar/数字类型.py
@@ -0,0 +1,21 @@
+# @Time    : 2018/10/5 12:16
+# @Author  : Yanlin Wang
+# @Email   : wangyl_a@163.com
+# @File    : 数字类型.py
+
+# 进制
+print(hex(78))  # 0x4x
+print(oct(78))  # 0o116
+print(int(78.8))  # 78
+print(bin(64))  # 0b1000000
+
+# eval() 解析字符串,将字符串作为Python代码解析
+print(eval('64'))
+print(eval('0o100'))
+print(eval('0x40'))
+print(eval('0b1000000'))  # 二进制
+
+# 二进制的位数 X.bit_length(); len(bin(X)) -2 减去0b标头
+# 其中 X.bit_length() 方法 计算效率最高
+X = 100
+print(X, bin(X), X.bit_length(), len(bin(X)) - 2)  # 100 0b1100100 7 7
