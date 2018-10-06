17ee33c2453b39c543827fff8af614667c70dc39
WangYanlin <wangyl_a@163.com>
2018-10-06 20:36:47 +0800

    重定向 输出流 sys.out

--- grammar/函数式编程.py
+++ grammar/函数式编程.py
@@ -15,3 +15,13 @@ L2 = list(map(lambda x: x + 1, list1))
 print(L2)
 L3 = list(map(lambda x, y: x * y, list1, list1))
 print(L3)  # [1, 9, 64, 49, 81, 4]
+
+# 命名规则
+# print([object,...][,sep=' '][,end='\n'][, file = sys.stdout])
+"""
+默认地发送到标准输出流,然而发送到其它文件也是有用的
+"""
+data = open('data.txt', 'w')
+print(L1, L2, L2, sep='---', file=data)
+data.close()
+print("hello, world!")
