3d3972f4654a6ba37fdafcf6899dd18abb616004
WangYanlin <wangyl_a@163.com>
2018-10-06 20:24:14 +0800

    增强表达式

---
+++ grammar/Python优化.py
@@ -0,0 +1,12 @@
+# @Time    : 2018/10/6 20:06
+# @Author  : Yanlin Wang
+# @Email   : wangyl_a@163.com
+# @File    : Python优化.py
+
+a = 10
+# 增强语句
+"""
+增强语句,a只出现1次,只需计算一次,更加快速
+"""
+a += 10
+print(a)
