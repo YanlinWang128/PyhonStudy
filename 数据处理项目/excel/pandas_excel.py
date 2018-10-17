7df9c3253b2dafad2a9be484744659e1a1ccd7e9
WangYanlin <wangyl_a@163.com>
2018-10-17 21:40:36 +0800

    excel

--- 数据处理项目/excel/pandas_excel.py
+++ 数据处理项目/excel/pandas_excel.py
@@ -4,10 +4,14 @@
 # @File    : pandas_excel.py
 
 import pandas as pd
+import xlrd
 
 
 def main():
-    pass
+    data = xlrd.open_workbook('0111.xls')  # 读取Excel数据 并提取出所需量x和y
+    table = data.sheets()[0]
+    y = table.col_values(3)   # 读取列数据
+    x = table.col_values(1)
 
 
 if __name__ == "__main__":
